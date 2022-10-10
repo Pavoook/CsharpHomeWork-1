f32a38252c17cfcc9184e8c5611ac1c39a2c71d9
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-10 23:53:47 +0500

    Added Exersize №8 Even from 1 to N

---
+++ Ex008_Even1toN/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите верхнюю границу для ряда положительных четных чисел:"); // ввод числа
+string value = Console.ReadLine();
+int number = int.Parse(value);
+
+int count = 2; // вывод ряда положительных четных чисел, кроме последнего, через зяпятую
+while (number > count + 1){
+    Console.Write(count);
+    Console.Write(", ");
+    count = count + 2;
+}
+
+if (number % 2 == 0) Console.Write(number); // отдельный вывод последнего члена ряда, чтобы в конце ряда не было запятой
+else if (number > 1) Console.Write(number - 1);
