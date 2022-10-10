057ff8d278f25a85cd1b450e5fe1e30a84da5999
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-10 20:58:47 +0500

    Added Exersize №2 Max of Two Numbers

--- Ex002_MaxOfTwo/Program.cs
+++ Ex002_MaxOfTwo/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число для сравнения:");
+string value = Console.ReadLine();
+int N1 = int.Parse(value);
+
+Console.WriteLine("Введите второе число для сравнения:");
+value = Console.ReadLine();
+int N2 = int.Parse(value);
+
+if (N1 > N2){
+    Console.Write("Большее из чисел: ");
+    Console.WriteLine(N1);
+}
+else{
+    if (N2 > N1){
+        Console.Write("Большее из чисел: ");
+        Console.WriteLine(N2);
+    }
+    else{
+        Console.WriteLine("Числа равны.");
+    }
+}
