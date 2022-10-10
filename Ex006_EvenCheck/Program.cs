70c2c35b9b13ae74b7272c80253d675a9aa3a50a
Andrey Khudyakov <avh_2004@mail.ru>
2022-10-10 22:36:52 +0500

    Added Exersize №6 Even Check

---
+++ Ex006_EvenCheck/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число для проверки на четность:");
+string value = Console.ReadLine();
+int number = int.Parse(value);
+
+if (number % 2 == 0) Console.WriteLine("Число четное.");
+else Console.WriteLine("Число нечетное.");
